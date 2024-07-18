14775a337ae3effe20d4a7c6a30099bfecae866e
lgy159 <lgy159@naver.com>
2024-07-19 06:46:11 +0900

    도중 저장

---
+++ lib/data/model/member/login/login_info.dart
@@ -0,0 +1,22 @@
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'login_info.freezed.dart';
+
+part 'login_info.g.dart';
+
+@freezed
+class LoginInfo with _$LoginInfo {
+  const factory LoginInfo({
+    @JsonKey(name: 'member_id')
+    required String id,
+    @JsonKey(name: 'member_pw')
+    required String password,
+    @JsonKey(name: 'gcm_key')
+    required String fcmKey,
+    @JsonKey(name: 'device_os')
+    required String deviceOs,
+
+  }) = _LoginInfo;
+
+  factory LoginInfo.fromJson(Map<String, Object?> json) => _$LoginInfoFromJson(json);
+}
---
+++ lib/presentation/worksheet_screen/home_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class WorkSheetScreen extends StatelessWidget {
+  const WorkSheetScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
